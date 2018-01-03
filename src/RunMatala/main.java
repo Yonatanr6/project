808cf92abe8bdad16098821af56d4de68defc615
Yoni <Yoni@LAPTOP-FLQV87B9>
2018-01-03 21:22:49 +0200

    1

--- src/RunMatala/main.java
+++ src/RunMatala/main.java
@@ -4,7 +4,14 @@ import java.awt.List;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.nio.file.FileSystem;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.WatchKey;
+import java.nio.file.WatchService;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 import Algorithms.Algo2;
 import Tools.Writer;
@@ -23,6 +30,9 @@ public class main {
 		 * @author Shiran &Yonatan
 		 *
 		 */
+		try (WatchService service = FileSystems.getDefault().newWatchService()){
+			Map<WatchKey, Path> KeyMap = new HashMap<>();
+		}
 		String path=("C:\\Users\\Yoni\\git\\matala-shiran-yonatan-\\Data\\output\\Filter\\after_filter.csv");
 		
 		Read data=new Read();
